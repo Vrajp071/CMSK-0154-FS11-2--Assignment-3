64b4219fa7f88750bd68a9f1c712552f51c6c057
Vraj Patel <155692667+Vrajp071@users.noreply.github.com>
2024-03-28 16:40:30 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿public class BaseClass
+{
+    public virtual bool IsValidated()
+    {
+        return false;
+    }
+}
+
+public class Person : BaseClass
+{
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    public Person(string firstName = "", string lastName = "")
+    {
+        FirstName = firstName;
+        LastName = lastName;
+
+        if (FirstName == null) FirstName = "";
+        if (LastName == null) LastName = "";
+    }
+
+    public override bool IsValidated()
+    {
+        if (FirstName.Length > 0 && LastName.Length > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person bob = new Person();
+        Console.WriteLine(bob.IsValidated());
+
+        Person kim = new Person("kim", "smith");
+        Console.WriteLine(kim.IsValidated());
+    }
+}
